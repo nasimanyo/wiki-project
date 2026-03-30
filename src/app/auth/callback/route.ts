a8ab05f590cc6c2913b50ea470391dc320b83b8a
nasiman <nasimanyo1209@gmail.com>
2026-03-30 13:49:27 +0900

    エラーが発生する問題を修正中

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -5,7 +5,6 @@ import type { NextRequest } from 'next/server'
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/'
 
   if (code) {
@@ -14,17 +13,27 @@ export async function GET(request: NextRequest) {
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
-          getAll() {
-            return request.cookies.getAll()
+          get(name: string) {
+            return request.cookies.get(name)?.value
           },
-          setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              request.cookies.set(name, value))
-            NextResponse.next({
-              request: {
-                headers: request.headers,
-              },
-            }).cookies.setAll(cookiesToSet)
+          set(name: string, value: string, options: any) {
+            // If the response is not created yet, create it
+            const response = NextResponse.next()
+            response.cookies.set({
+              name,
+              value,
+              ...options,
+            })
+            return response
+          },
+          remove(name: string, options: any) {
+            const response = NextResponse.next()
+            response.cookies.set({
+              name,
+              value: '',
+              ...options,
+            })
+            return response
           },
         },
       }
@@ -32,19 +41,10 @@ export async function GET(request: NextRequest) {
 
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === 'development'
-      if (isLocalEnv) {
-        // We can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
-      }
+      return NextResponse.redirect(`${origin}${next}`)
     }
   }
 
-  // return the user to an error page with instructions
+  // Return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }
